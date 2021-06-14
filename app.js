1bab653cd784e12c1cc5c88508a8f2d966ac769d
clocktower39 <clocktower39@aol.com>
2021-06-14 12:43:12 -0700

    upload using GridFS

--- app.js
+++ app.js
@@ -1,26 +1,106 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const crypto = require('crypto');
+const path = require('path');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const mongoURI = process.env.ATLAS_URI;
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('mongo DB success');
+// connection
+const conn = mongoose.createConnection(mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+let gfs;
+conn.once("open", () => {
+  // init stream
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "uploads"
+  });
 });
 
-const userRouter = require('./routes/user');
-app.use('/users', userRouter);
+// Storage
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString("hex") + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: "uploads"
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  
+  const upload = multer({
+    storage
+  });
+
+  // get / page
+app.get("/", (req, res) => {
+    if(!gfs) {
+      console.log("some error occured, check connection to db");
+      res.send("some error occured, check connection to db");
+      process.exit(0);
+    }
+    gfs.find().toArray((err, files) => {
+      // check if files
+      if (!files || files.length === 0) {
+        return res.render("index", {
+          files: false
+        });
+      } else {
+        const f = files
+          .map(file => {
+            if (
+              file.contentType === "image/png" ||
+              file.contentType === "image/jpeg"
+            ) {
+              file.isImage = true;
+            } else {
+              file.isImage = false;
+            }
+            return file;
+          })
+          .sort((a, b) => {
+            return (
+              new Date(b["uploadDate"]).getTime() -
+              new Date(a["uploadDate"]).getTime()
+            );
+          });
+  
+        // return res.render("index", {
+        //   files: f
+        // });
+      }
+  
+      return res.json(files);
+    });
+  });
+
+  app.post("/upload", upload.single("file"), (req, res) => {
+    res.redirect("/");
+  });
+  
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- routes/user.js
+++ routes/user.js
@@ -1,45 +1,76 @@
-const router = require('express').Router();
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-let path = require('path');
-let User = require('../models/user.modal');
+const router = require("express").Router();
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const methodOverride = require("method-override");
+const { v4: uuidv4 } = require("uuid");
+let path = require("path");
+let User = require("../models/user.modal");
+const { resolve } = require("path");
+const crypto = require("crypto");
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'images');
-    },
-    filename: function(req, file, cb) {   
-        cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
-    }
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, 'images');
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
+//     }
+// });
+
+const storage2 = new GridFsStorage({
+  url: process.env.ATLAS_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(dile.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
 });
-
 const fileFilter = (req, file, cb) => {
-    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
-    if(allowedFileTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(null, false);
+  const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedFileTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// let upload = multer({ storage, fileFilter });
+let upload = multer({ storage2 });
+
+router.route("/add").post(upload.single("file"), (req, res, next) => {
+  const name = req.body.name;
+  const birthdate = req.body.birthdate;
+  const photo = req.file.filename;
+
+  User.findOne({ photo: photo }).then((image) => {
+    console.log(image);
+    if (image) {
+      return res.status(200).json({
+        success: false,
+        message: "Image already exists",
+      });
     }
-}
-
-let upload = multer({ storage, fileFilter });
-
-router.route('/add').post(upload.single('photo'), (req, res) => {
-    const name = req.body.name;
-    const birthdate = req.body.birthdate;
-    const photo = req.file.filename;
-
     const newUserData = {
-        name,
-        birthdate,
-        photo
-    }
-
+      name,
+      birthdate,
+      photo,
+    };
     const newUser = new User(newUserData);
-
-    newUser.save()
-           .then(() => res.json('User Added'))
-           .catch(err => res.status(400).json('Error: ' + err));
+    newUser
+      .save()
+      .then(() => res.json("User Added"))
+      .catch((err) => res.status(400).json("Error: " + err));
+  });
 });
 
 module.exports = router;
